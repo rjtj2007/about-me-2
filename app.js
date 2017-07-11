91cf9ec2e30a6bac3445a44749bc310bcf5523be
Ron Barrantes <ronbarrantes@gmail.com>
2017-07-11 16:06:13 -0700

    sketched out all the questions

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict';
 // type some questions with answers to who am I as a little intro, questions should be fun
 
-var tacos = prompt('Does Ron likes tacos');
-console.log('the user input ' + tacos);
+var tacos = prompt('Does Ron likes tacos?');
+console.log('The user answered ' + tacos);
 if (tacos.toLowerCase() == 'yes') {
   alert('Absolutely, Ron really likes tacos');
 } else {
@@ -11,5 +11,20 @@ if (tacos.toLowerCase() == 'yes') {
 
 
 /*
+Does Ron likes coffee?
+A1 Yah... Ron's veins are overflown with coffee.
+A2. Ron requires coffee to survive.
+
+Does Ron likes animals?
+A1. Yes! Ron loves animals, his spirit animal is a Pomeranian
+A2. In fact Ron loves animals, his spirit animal is a Pomeranian
+
+Does Ron likes Seattle traffic?
+A1. The correct answer is No. Seattle traffic is the worst!!
+A2. That's correct! Ron hates Seattle traffic. Do you like seattle traffic?
+
+Does Ron likes Seattle?
+A1. Yes Ron loves Seattle
+A2. Actually, Ron thinks Seattle is great!
 
 */
