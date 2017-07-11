430629269de48965699e316382a0200c8cd61e62
Ron Barrantes <ronbarrantes@gmail.com>
2017-07-11 15:49:01 -0700

    added the first question, question working

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-'use strict'
+'use strict';
+// type some questions with answers to who am I as a little intro, questions should be fun
+
+var tacos = prompt('Does Ron likes tacos');
+console.log('the user input ' + tacos);
+if (tacos.toLowerCase() == 'yes') {
+  alert('Absolutely, Ron really likes tacos');
+} else {
+  alert('Actually, Ron really likes tacos');
+}
+
+
+/*
+
+*/
