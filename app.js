81ff7a684164b6a5994fff8132f72b9a6c89a03c
Ron Barrantes <ronbarrantes@gmail.com>
2017-07-12 13:20:37 -0700

    minor modifications during class

--- app.js
+++ app.js
@@ -3,35 +3,35 @@
 
 var tacos = prompt('Does Ron likes tacos?');
 console.log('The user answered ' + tacos);
-if (tacos.toLowerCase() == 'yes') {
+if (tacos.toLowerCase() === 'yes') {
   alert('Absolutely, Ron really likes tacos');
 } else {
   alert('Actually, Ron really likes tacos');
 }
 
 var coffee = prompt('Does Ron likes coffee?');
-if (coffee.toLowerCase() == 'yes') {
+if (coffee.toLowerCase() === 'yes') {
   alert('Yah... Ron\'s veins are overflown with coffee');
 } else {
   alert('Ron requires coffee to survive');
 }
 
 var animals = prompt('Does Ron likes animals?');
-if (animals.toLowerCase() == 'yes') {
+if (animals.toLowerCase() === 'yes') {
   alert('Yes! Ron loves animals, his spirit animal is a Pomeranian');
 } else {
   alert('In fact Ron loves animals, his spirit animal is a Pomeranian');
 }
 
 var traffic = prompt('Does Ron likes Seattle traffic?');
-if (traffic.toLowerCase() == 'yes') {
+if (traffic.toLowerCase() === 'yes') {
   alert('The correct answer is No. Seattle traffic is the worst!!');
 } else {
   alert('That\'s correct! Ron hates Seattle traffic. Do you like seattle traffic?');
 }
 
 var city = prompt('Does Ron likes Seattle?');
-if (city.toLowerCase() == 'yes') {
+if (city.toLowerCase() === 'yes') {
   alert('Yes, Ron loves Seattle');
 } else {
   alert('Actually, Ron thinks Seattle is great!');
