44597be715eb915cda7676fb219bc5e387cbc758
Ron Barrantes <ronbarrantes@gmail.com>
2017-07-12 17:06:22 -0700

    questionaire nearly completed

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 
 var score = 0;
 var counter = 0;
+var food = '';
 
 var question = [
   'Does Ron likes tacos?',
@@ -16,17 +17,29 @@ var question = [
 
 // first item inside answer is correct, second item is incorrect, third item is user response
 var answer = [
-  ['Absolutely, Ron really likes tacos', 'Actually, Ron really likes tacos', 'yes' || 'y'],
-  ['Yah... Ron\'s veins are overflown with coffee', 'Ron requires coffee to survive', 'yes' || 'y'],
-  ['Yes! Ron loves animals, his spirit animal is a Pomeranian', 'In fact Ron loves animals, his spirit animal is a Pomeranian', 'yes' || 'y'],
-  ['That\'s correct! Ron hates Seattle traffic. Do you like seattle traffic?', 'The correct answer is No. Seattle traffic is the worst!!', 'no' || 'n'],
-  ['Yes, Ron loves Seattle', 'Actually, Ron thinks Seattle is great!', 'yes' || 'y'],
+  ['Absolutely, Ron really likes tacos', 'Actually, Ron really likes tacos', 'yes'],
+  ['Yah... Ron\'s veins are overflown with coffee', 'Ron requires coffee to survive', 'yes'],
+  ['Yes! Ron loves animals, his spirit animal is a Pomeranian', 'In fact Ron loves animals, his spirit animal is a Pomeranian', 'yes'],
+  ['That\'s correct! Ron hates Seattle traffic. Do you like seattle traffic?', 'The correct answer is No. Seattle traffic is the worst!!', 'no'],
+  ['Yes, Ron loves Seattle', 'Actually, Ron thinks Seattle is great!', 'yes'],
   Math.ceil(Math.random() * 20), ['pizza', 'burgers', 'sushi', 'steak']
 ];
 
+var funResponse = [
+  'I don\' think you\'ve ever met me before ',
+  'Seriously!!!',
+  'Yeah! No! You\'re just guessing',
+  'Ok, I think you\'ve seen me around',
+  'I\'d figure you\'d know me better than that',
+  'Hey!!! not bad!',
+  'Did we go to High School together?',
+  'Wow!!! What a stalker!!! Yo'
+];
+
+
 ///// GAME /////
 // yes and no questions?
-/*
+
 for (var i = 0; i < 5; i++) {
   var response = prompt(question[i]);
   if (response.toLowerCase() === answer[i][2]) {
@@ -37,73 +50,51 @@ for (var i = 0; i < 5; i++) {
     alert(answer[i][1]);
   }
 }
-*/
-/*
-console.log(answer[5]);
 
 // Guess the number
-while (counter < 4){
-  console.log (counter);
+while (counter < 4) {
   var guess = parseInt(prompt(question[5]));
   // if too high
   if (guess > answer[5]) {
-    console.log('too high ', guess);
+    alert(guess + ' is too high');
+    console.log('User input too high ', guess);
   }
   // if too low
   else if (guess < answer[5]) {
-    console.log('too low ', guess);
+    alert(guess + ' is too low');
+    console.log('User input too low ', guess);
   }
   // if correct
   else if (guess === answer[5]) {
-    console.log('That\'s correct!!! ', guess);
+    alert('That\'s correct!!! ' + guess + ' is the right answer!');
     score++;
-    // counter = 7; // super hack way of doint it
+    console.log('Correct response of ', guess);
     break;
   }
   counter++;
-  console.log(counter);
 }
+// counter reset
 counter = 0;
 
-*/
-
+// Guess food game
 console.log(answer[6]);
-
 while (counter < 7) {
   console.log(counter);
-  var food = prompt(question[6]).toLowerCase();
-  food = 'pizza';
-  for (var i = 0; i < answer[6].length; i++) {
-    console.log(answer[6][i]);
-    if (answer[6][i] === food) {
-      console.log('That\'s right, I actually love pizza, burgers, sushi, and steak');
+  if (counter === 0) {
+    food = prompt(question[6]).toLowerCase();
+  } else {
+    food = prompt('Please try again! ' + question[6]).toLowerCase();
+  }
+  for (var j = 0; j < answer[6].length; j++) {
+    if (answer[6][j] === food) {
+      alert('That\'s right, I actually love pizza, burgers, sushi, and steak');
+      score++;
       counter = 7;
-    } else {
-      console.log('Please try again')
     }
-
   }
   counter++;
-  //console.log(counter, food);
 }
 
+alert('Your score is ' + score + ' out of ' + question.length + '. ' + funResponse[score]);
 
-// Pick one
 console.log('Your score is ' + score + ' out of ' + question.length);
-
-
-/*
-var traffic = prompt('Does Ron likes Seattle traffic?');
-if (traffic.toLowerCase() === 'yes') {
-  alert('The correct answer is No. Seattle traffic is the worst!!');
-} else {
-  alert('That\'s correct! Ron hates Seattle traffic. Do you like seattle traffic?');
-}
-
-var city = prompt('Does Ron likes Seattle?');
-if (city.toLowerCase() === 'yes') {
-  alert('Yes, Ron loves Seattle');
-} else {
-  alert('Actually, Ron thinks Seattle is great!');
-}
-*/
